8457df6f76e817f33e949d9cb354fbdaf6109c83
islTrea <110476159+islTrea@users.noreply.github.com>
2022-08-24 00:11:32 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,35 @@
 from telethon import TelegramClient, events, sessions
 import logging
+from decouple import config
+from iqoptionapi.stable_api import IQ_Option
 
+import time as t
+from datetime import datetime
+import datetime as dt
+###################
+UserName = config("IQ_UserName")
+UserPass = config("IQ_PWS")
 
+percentUsed = config("PERCENTAGE")
+Account_Type = config("ACCOUNT_TYPE")
+
+Iq=IQ_Option(UserName, UserPass)
+  
+iqch1, iqch2=Iq.connect()
+  	
+if iqch1==True:
+  print("IqOption Log In Successful.") 
+else:
+  print("IqOption Log In failed.")
+#################
 class vars:
 	debug = False
-	api_id = 10802903
-	api_hash = '3c274f7b6627d3e8bfe5c986ce20baca'
-	session_str = '1BJWap1wBu6VRIUHwqQQvJvT5uEzlOw4c3vO2cBJKFb7i27XlHsNQARhpsZVsRe2DW4XXOAAH1R0SWWfSFvm9TzennLz6cRuFiDtegdKvMYSPHOpD_Vbq89YPiX-B3C6rsF3S3QApNLUz4tBptixdV1scCYUOIKkIC4vaD1xkGEP6AOs4GNXzyirdgjHYyDk_HZH_PQ8B_opOK8vpn7ZF8kVQLPNaCOl5Th8Y-F7mxOLvSu66nI9f2NKiX8OZBHTsfMMY2J21z_8IZWOplwpsdxhJm-0Xx0nLyM3y22RYMpBx7bBCFTLLUXdk8h2rZL7YBUCfhuTB2lU1BOeFx56K__TQkHn0fOM='
-	from_chats = ["fxrashel",5094712949]
-  
+	api_id = config("APP_ID", default=None, cast=int)
+	api_hash = config("API_HASH", default=None)
+	session_str = config("SESSION")
+	from_chats = config("FROM_CHANNEL")
+	to_chats = config("TO_CHANNEL")
+  	
 logging.basicConfig(level=logging.DEBUG if vars.debug else logging.WARNING)
 
 app = TelegramClient(
@@ -23,11 +44,168 @@ app = TelegramClient(
 async def new_message(event):
   print(event.text) #this is the text what is being received and we got is printed!
   #print(event.date) #this is the Date what is being received and we got is printed!
+  
+  #################################
+  
+  if Iq.check_connect()==False: 
+  	iqch1, iqch2=Iq.connect()
+  	
+  	if iqch1==True:
+  		print("IqOption reconnect In Successful.") 
+  	else:
+  		print("IqOption reconnect failed.")
+  
+  
+  #Iq.change_balance("REAL")
+  
+  Iq.change_balance(Account_Type)
+  
+  my_blc=Iq.get_balance()
+  print(f"Balance: {my_blc} ")
   try:
-      await app.send_message(-1001553122017,event.message)
+      await app.send_message(to_chats , "Balance: "+str(my_blc))
   except Exception as e:
             print(e)
+  
+  #SET TRADE PARAMETERS 
+  #Money=10 #AMOUNT PER OPTION
+  ####################################################
+  #new_message = "EUR/JPY CALL 5 MINT WAIT CONFRIM"
+  new_message = event.text
+  
+  confirm = "WAIT CONFRIM"
+  ## percentage of balance to trade
+  percentOfBalance = int(percentUsed)
+  Money = float("{:.2f}".format(my_blc * (percentOfBalance/100)))
+  trade = ""
+  expirations_mode = ""
+  market = ""
+  tradeReady = False
+  tradeON = False
+  	
+  	
+  if tradeReady == False and confirm in new_message:
+  	tradeReady = True
+  	for z in new_message.split():
+  		if z.isdigit():
+  			expirations_mode=int(z)
+  	#print(minutes)
+  	##Direction
+  	if "CALL" in new_message:
+  		trade = "call"
+  	if "PUT" in new_message:
+  		trade = "put"
+  	##Curreny pair
+  	if 'EUR/USD' in new_message:
+  		market = 'EURUSD'
+  	if 'EUR/GBP' in new_message:
+  		market = 'EURGBP'
+  	if 'GBP/JPY' in new_message:
+  		market = 'GBPJPY'
+  	if 'EUR/JPY' in new_message:
+  		market = 'EURJPY'
+  	if 'GBP/USD' in new_message:
+  		market = 'GBPUSD'
+  	if 'USD/JPY' in new_message:
+  		market = 'USDJPY'
+  	if 'AUD/CAD' in new_message:
+  		market = 'AUDCAD'
+  	if 'NZD/USD' in new_message:
+  		market = 'NZDUSD'
+  	if 'USD/RUB' in new_message:
+  		market = 'USDRUB'
+  	if 'AUD/USD' in new_message:
+  		market = 'AUDUSD'
+  	if 'USD/CAD' in new_message:
+  		market = 'USDCAD'
+  	if 'AUD/JPY' in new_message:
+  		market = 'AUDJPY'
+  	if 'GBP/CAD' in new_message:
+  		market = 'GBPCAD'
+  	if 'GBP/CHF' in new_message:
+  		market = 'GBPCHF'
+  	if 'GBP/AUD' in new_message:
+  		market = 'GBPAUD'
+  	if 'EUR/CAD' in new_message:
+  		market = 'EURCAD'
+  	if 'CHF/JPY' in new_message:
+  		market = 'CHFJPY'
+  	if 'CAD/CHF' in new_message:
+  		market = 'CADCHF'
+  	if 'EUR/AUD' in new_message:
+  		market = 'EURAUD'
+  		
+  	#print(expirations_mode)
+  	#print(trade)
+  	#print(market)
+  
+  if tradeReady == True and "GO" in new_message:
+  	#trade using all the variable
+  	print("trade is active")
+  	#then clear variables
+  	tradeON = True
+  
+  ####################################################
+  #Money = 10
+  #market = "EURUSD" #TARGET INSTRUMENT
+  #expirations_mode=1 #EXPIRATION TIME IN MINUTES
+  #trade = "put"
+  #tradeOn = False
+  #LET'S DO SOMETHING 
+  #print("IQ Bot started ...")
+  
+  
+  while tradeON == True:	
+  	#CALL OPTION 
+  	if trade == "call":
+  	#CALL OPTION 
+  		app.send_message(to_chats,"Trade: "+trade + " Currency Pair: "+market+ " Expiry: "+expirations_mode+" Min "+ " $ "+Money )
+  		print("Green")
+  		check, id=Iq.buy (Money, market, "call", expirations_mode)
+  		if check:
+  			print("'CALL' Option Placed.") 
+  			print("Awaiting 'Call Option Result...") 
+  			#FUNCTION TO GET OPTION RESULT
+  			if(Iq.check_win_v3(id) !=""):
+  				tradeON = False
+  				tradeON = False
+  				trade = ""
+  				minutes = ""
+  				market = ""
+  				app.send_message(to_chats , "Win/Loss: " + Iq.check_win_v3(id) + "New Balance: "+Iq.get_balance() )
+  				print(Iq.check_win_v3(id)) 
+  		else:
+  			tradeON = False
+  			tradeReady == False
+  			app.send_message(to_chats , " 'Put' Option Failed.")
+  			print("Call Option Failed.") 
+  	 		
+  	elif trade == "put":
+  	#PUT OPTION 
+  		app.send_message(to_chats , "Trade: "+trade + " Currency Pair: "+market+ " Expiry: "+expirations_mode+" Min "+ " $ "+Money )
+  		print("Red") 
+  		check, id=Iq. buy (Money, market, "put", expirations_mode)
+  		if check:
+  			print("'PUT' Option Placed.") 
+  			print("Awaiting 'Put' Option Result...") 
+  			#FUNCTION TO GET OPTION RESULT
+  			if(Iq.check_win_v3(id) !=""):
+  				tradeON = False
+  				trade = ""
+  				minutes = ""
+  				market = ""
+  				app.send_message(to_chats , "Win/Loss: " + Iq.check_win_v3(id)+ "New Balance: "+Iq.get_balance() )
+  				print(Iq.check_win_v3(id))  
+  		else: 
+  			tradeON = False
+  			tradeReady == False
+  			app.send_message(to_chats , " 'Put' Option Failed.")
+  			print(" 'Put' Option Failed.")
+  
 
+  #################################
+	#print(event.text) #this is the text what is being received and we got is printed!
+  #print(event.date) #this is the Date what is being received and we got is printed!
 if __name__ == "__main__":
 	print("TGram Bot started!!")
 	app.run_until_disconnected()
