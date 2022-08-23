62a967a3d0a178701f4e4c703a6b4e234dd392c3
islTrea <110476159+islTrea@users.noreply.github.com>
2022-08-23 22:52:31 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,102 +1,33 @@
-#    Copyright (c) 2021 Ayush
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
-
-from telethon import TelegramClient, events
-from decouple import config
+from telethon import TelegramClient, events, sessions
 import logging
-from telethon.sessions import StringSession
 
-from iqoptionapi.stable_api import IQ_Option 
-import time
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+class vars:
+	debug = False
+	api_id = 10802903
+	api_hash = '3c274f7b6627d3e8bfe5c986ce20baca'
+	session_str = '1BJWap1wBu6VRIUHwqQQvJvT5uEzlOw4c3vO2cBJKFb7i27XlHsNQARhpsZVsRe2DW4XXOAAH1R0SWWfSFvm9TzennLz6cRuFiDtegdKvMYSPHOpD_Vbq89YPiX-B3C6rsF3S3QApNLUz4tBptixdV1scCYUOIKkIC4vaD1xkGEP6AOs4GNXzyirdgjHYyDk_HZH_PQ8B_opOK8vpn7ZF8kVQLPNaCOl5Th8Y-F7mxOLvSu66nI9f2NKiX8OZBHTsfMMY2J21z_8IZWOplwpsdxhJm-0Xx0nLyM3y22RYMpBx7bBCFTLLUXdk8h2rZL7YBUCfhuTB2lU1BOeFx56K__TQkHn0fOM='
+	from_chats = ["fxrashel",5094712949]
+  
+logging.basicConfig(level=logging.DEBUG if vars.debug else logging.WARNING)
 
-print("Starting...")
+app = TelegramClient(
+	session=sessions.StringSession(vars.session_str),#if you have a session string, then you can use above version!
+	#if you want a local sesion, then just a name would be fine here. (commented below)
+	#session="mySession",
+	api_id=vars.api_id,
+	api_hash=vars.api_hash
+).start()
 
-# Basics
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-SESSION = config("SESSION")
-FROM_ = config("FROM_CHANNEL")
+@app.on(events.NewMessage(incoming=True, chats=vars.from_chats))
+async def new_message(event):
+  print(event.text) #this is the text what is being received and we got is printed!
+  #print(event.date) #this is the Date what is being received and we got is printed!
+  try:
+      await app.send_message(-1001553122017,event.message)
+  except Exception as e:
+            print(e)
 
-USER = config("IQ_UserName")
-     
-PWS = config("IQ_PWS")
-      
-FROM = [int(i) for i in FROM_.split()]
-
-##
-API = IQ_Option(USER, PWS)
-ch1, ch2 = API.connect()
-
-if ch1 == True:
-	print("Login Successfull !") 
-else:
-	print("Login Failed !") 
-	quit()
-	
-##	
-try:
-    BotzHubUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
-    BotzHubUser.start()
-except Exception as ap:
-    print(f"ERROR - {ap}")
-    exit(1)
-
-    
-    
-ACTIVES="EURUSD"
-Money=1
-order = "put"
-receivedHTM = "GO"
-
-@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
-async def sender_bH(event):
-    while True:
-        #if receivedHTM in (event.message).lower():
-	#if "A" == "A":
-	    #if receivedHTM == "GO":
-	    if "test" == "test":
-		if order == "put":
-		#ACTION_call="put"
-			
-        	    #order_sell,id_put=I_want_money.buy(Money,ACTIVES,ACTION_call,expirations_mode)
-		    order_put,id_put = API.buy_digital_spot("EURUSD",1, "put",1) 
-		    print("Order Put")
-		    if order_put == True: 
-			while True:
-			    check_put, win_put = API.check_win_digital_v2(id_put) 
-			    if check_put == True:
-			 	break
-			if win_put > 0:
-			    print("Put win") 
-			else:
-			    print("Put LOSE")
-				
-		if order == "call":
-		#ACTION_call="call"
-        	    #order_buy,id_call=I_want_money.buy(Money,ACTIVES,ACTION_call,expirations_mode)
-		    order_call,id_call = API.call_digital_spot("EURUSD",1, "call",1) 
-		    print("Order Put")
-		    if order_call == True: 
-			while True:
-			    check_call, win_call = API.check_win_digital_v2(id_call) 
-			    if check_call == True:
-				break
-			    if win_call > 0:
-				print("Call win") 
-			    else:
-				print("Call LOSE")
-
-print("Bot has started.")
-BotzHubUser.run_until_disconnected()
+if __name__ == "__main__":
+	print("TGram Bot started!!")
+	app.run_until_disconnected()
