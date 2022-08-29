2703541c348189bd07bdd105a84ce5d51cf0387e
islTrea <110476159+islTrea@users.noreply.github.com>
2022-08-29 23:49:57 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,12 @@
 from telethon import TelegramClient, events, sessions
 import logging
 from decouple import config
+
+import time
 from iqoptionapi.stable_api import IQ_Option
-import requests
+from datetime import date
 
-import time as t
-from datetime import datetime
-import datetime as dt
 ###################
-def send_msg(text):
-  token = "5723766375:AAEGKMl4rRjt_ODYGkiBSYcIaV5VBlvUxXQ"
-  chat_id = "5007081209"
-  url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
-  results = requests.get(url_req)
-  print(results.json())
-
 UserName = config("IQ_UserName")
 UserPass = config("IQ_PWS")
 
@@ -26,207 +18,167 @@ Iq=IQ_Option(UserName, UserPass)
 iqch1, iqch2=Iq.connect()
   	
 if iqch1==True:
-  print("IqOption Log In Successful.") 
-  send_msg("Hello there!")
+  print("IqOption Log In Successful!!") 
 else:
-  print("IqOption Log In failed.")
-  send_msg("Hello there!")
-#################
-
-
+  print("IqOption Log In failed!!")
+  
+##################
 class vars:
-	debug = False
-	api_id = config("APP_ID", default=None, cast=int)
-	api_hash = config("API_HASH", default=None)
-	session_str = config("SESSION")
-	
-logging.basicConfig(level=logging.DEBUG if vars.debug else logging.WARNING)
+  debug = False
+  api_id = config("APP_ID", default=None, cast=int)
+  api_hash = config("API_HASH", default=None)
+  session_str = config("SESSION")
+  
 
+logging.basicConfig(level=logging.DEBUG if vars.debug else logging.WARNING)
 FROM_ = config("FROM_CHANNEL")
 to_chats = config("TO_CHANNEL")
-
-from_chats = [int(i) for i in FROM_.split()]
-
 app = TelegramClient(
-  session=sessions.StringSession(vars.session_str),#if you have a session string, then you can use above version!
+  session='test2',#if249 you have a session string, then you can use above version!
 	#if you want a local sesion, then just a name would be fine here. (commented below)
 	#session="mySession",
   api_id=vars.api_id,
   api_hash=vars.api_hash
 ).start()
 
-@app.on(events.NewMessage(incoming=True, chats=from_chats))
+from_chats = [int(i) for i in FROM_.split()]
+
+@app.on(events.NewMessage(incoming=True, chats=vars.from_chats))
 async def new_message(event):
-  print(event.text) #this is the text what is being received and we got is printed!
-  #print(event.date) #this is the Date what is being received and we got is printed!
-  send_msg(event.text)
-  #################################
-  
-  
-  #Iq.change_balance("REAL")
-  
+  print("Message: "+event.text) 
+  today = date.today()
+ 
   Iq.change_balance(AccountType)
-  
+
   my_blc=Iq.get_balance()
   try:
-    await app.send_message(to_chats , "Account Type: "+AccountType+" Balance: "+str(my_blc))  
+    await app.send_message(to_chats , "Message: "+event.text)  
   except Exception as e:
     print(e)
-  print(f"Balance: {my_blc} ")
   
-  #SET TRADE PARAMETERS 
-  #Money=10 #AMOUNT PER OPTION
-  ####################################################
-  #new_message = "EUR/JPY CALL 5 MINT WAIT CONFRIM"
-  new_message = event.text
-  
+  ############################
+  newmessage = str(event.text)
+
   confirm = "WAIT CONFRIM"
   ## percentage of balance to trade
-  percentOfBalance = int(percentUsed)
+  percentOfBalance = percentUsed
   Money = float("{:.2f}".format(my_blc * (percentOfBalance/100)))
   trade = ""
   expirations_mode = ""
-  market = ""
-  tradeReady = False
-  tradeON = False
+  market = "" 
   	
-  	
-  if tradeReady == False and confirm in new_message:
-    tradeReady = True
-    for z in new_message.split():
+  if confirm in newmessage:
+    try:
+      await app.send_message(to_chats , "IqOption: Wait Confirm active")  
+    except Exception as e:
+      print(e)
+    print("IqOption: Wait Confirm active")
+    
+    for z in newmessage.split():
       if z.isdigit():
         expirations_mode=int(z)
     #print(minutes)
     ##Direction
-    if "CALL" in new_message:
+    if "call" in newmessage.lower():
       trade = "call"
-    if "PUT" in new_message:
+      print("call detected")
+    if "put" in newmessage.lower():
       trade = "put"
-    ##Curreny pair
-    if 'EUR/USD' in new_message:
-      market = 'EURUSD'
-    if 'EUR/GBP' in new_message:
-      market = 'EURGBP'
-    if 'GBP/JPY' in new_message:
-      market = 'GBPJPY'
-    if 'EUR/JPY' in new_message:
-      market = 'EURJPY'
-    if 'GBP/USD' in new_message:
-      market = 'GBPUSD'
-    if 'USD/JPY' in new_message:
-      market = 'USDJPY'
-    if 'AUD/CAD' in new_message:
-      market = 'AUDCAD'
-    if 'NZD/USD' in new_message:
-      market = 'NZDUSD'
-    if 'USD/RUB' in new_message:
-      market = 'USDRUB'
-    if 'AUD/USD' in new_message:
-      market = 'AUDUSD'
-    if 'USD/CAD' in new_message:
-      market = 'USDCAD'
-    if 'AUD/JPY' in new_message:
-      market = 'AUDJPY'
-    if 'GBP/CAD' in new_message:
-      market = 'GBPCAD'
-    if 'GBP/CHF' in new_message:
-      market = 'GBPCHF'
-    if 'GBP/AUD' in new_message:
-      market = 'GBPAUD'
-    if 'EUR/CAD' in new_message:
-      market = 'EURCAD'
-    if 'CHF/JPY' in new_message:
-      market = 'CHFJPY'
-    if 'CAD/CHF' in new_message:
-      market = 'CADCHF'
-    if 'EUR/AUD' in new_message:
-      market = 'EURAUD'
-  		
-    #print(expirations_mode)
-    #print(trade)
-    #print(market)
+      print("put")
+    pairList ={'EUR/USD':'EURUSD','EUR/GBP':'EURGBP','GBP/JPY':'GBPJPY'
+               ,'EUR/JPY':'EURJPY','GBP/USD':'GBPUSD','USD/JPY':'USDJPY'
+               ,'AUD/CAD':'AUDCAD','NZD/USD':'NZDUSD','USD/RUB':'USDRUB'
+               ,'AUD/USD':'AUDUSD','USD/CAD':'USDCAD','AUD/JPY':'AUDJPY'
+               ,'GBP/CAD':'GBPCAD','GBP/CHF':'GBPCHF','GBP/AUD':'GBPAUD'
+               ,'EUR/CAD':'EURCAD','CHF/JPY':'CHFJPY','CAD/CHF':'CADCHF'
+               ,'EUR/AUD':'EURAUD'
+              }  
+    for pair in pairList:
+    	if pair in newmessage:
+    		#print(pairList[pair])
+  		  market = pairList[pair]
+    
+    try:
+      await app.send_message(to_chats ,today+" -IqOption: Market Active: "+market+" Expiry: "+str(expirations_mode)+" Account Type: "+Account_Type+" Balance: "+str(my_blc))  
+    except Exception as e:
+      print(e)
+    print("IqOption: Market Active: "+market+ " Expiry: "+str(expirations_mode))
   
-  if tradeReady == True and "GO" in new_message:
-    #trade using all the variable
-    print("trade is active")
-    #then clear variables
-    tradeON = True
-  
-  ####################################################
-  #Money = 10
-  #market = "EURUSD" #TARGET INSTRUMENT
-  #expirations_mode=1 #EXPIRATION TIME IN MINUTES
-  #trade = "put"
-  #tradeOn = False
-  #LET'S DO SOMETHING 
-  #print("IQ Bot started ...")
+    
+  if "go" in newmessage.lower():
+    try:
+      await app.send_message(to_chats ,today+" -IqOption: GO is Active: ")  
+    except Exception as e:
+      print(e)
+    print("IqOption: GO is Active: ")
 
-  while tradeON == True:	
     #CALL OPTION 
     if trade == "call":
     #CALL OPTION 
       try:
-        await app.send_message(to_chats,"Trade: "+trade + " Currency Pair: "+market+ " Expiry: "+str(expirations_mode)+" Min "+ " $ "+str(Money) )  
+        await app.send_message(to_chats,"IqOption: Trade: "+trade + " Currency Pair: "+market+ " Expiry: "+str(expirations_mode)+" Min "+ " $ "+str(Money) )  
       except Exception as e:
         print(e)
+      
       print("Green")
-      check, id=Iq.buy (Money, market, "call", expirations_mode)
+      check, id=Iq.buy (float(Money), market, "call", int(expirations_mode))
       if check:
         print("'CALL' Option Placed.") 
-        print("Awaiting 'Call Option Result...") 
-        await app.send_message(to_chats , "Awaiting 'Call Option Result...")
+        print("IqOption: Awaiting 'Call Option Result...") 
+        try:
+          await app.send_message(to_chats , "IqOption: Awaiting 'Call Option Result...")
+        except Exception as e:
+            print(e)
         #FUNCTION TO GET OPTION RESULT
         if(Iq.check_win_v3(id) !=""):
-          tradeON = False
           trade = ""
-          minutes = ""
+          expirations_mode = ""
           market = ""
-          try:
-            await app.send_message(to_chats , "Win/Loss: " + Iq.check_win_v3(id) + "New Balance: "+str(Iq.get_balance()) )  
-          except Exception as e:
-            print(e)  
-          print(Iq.check_win_v3(id)) 
-      else:
-        tradeON = False
-        tradeReady == False
-        await app.send_message(to_chats , " 'Put' Option Failed.")
-        print("Call Option Failed.") 
+        if(Iq.check_win_v3(id) > 0):
+          print("Win ✅")
+          await app.send_message(to_chats , today+" - IqOption: Win ✅: " + str(float(Iq.check_win_v3(id))) + " IqOption: New Balance: "+str(Iq.get_balance()) )  
+          print(float(Iq.check_win_v3(id))) 
+        elif(Iq.check_win_v3(id) < 0):
+          print("Loss ⛔️") 
+          await app.send_message(to_chats , today+" -IqOption: Loss ⛔️: " + str(float(Iq.check_win_v3(id))) + " IqOption: New Balance: "+str(Iq.get_balance()) )  
+          print(float(Iq.check_win_v3(id))) 
+      else:  
+        await app.send_message(to_chats , today+" -IqOption: 'Put' Option Failed.")
+        print("IqOption: Call Option Failed.") 
   	 		
     elif trade == "put":
   	#PUT OPTION 
       try:
-        await app.send_message(to_chats,"Trade: "+trade + " Currency Pair: "+market+ " Expiry: "+str(expirations_mode)+" Min "+ " $ "+str(Money) )  
+        await app.send_message(to_chats, today+" -IqOption: Trade: "+trade + " Currency Pair: "+market+ " Expiry: "+str(expirations_mode)+" Min "+ " $ "+str(Money) )  
       except Exception as e:
         print(e)
       print("Red") 
-      check, id=Iq. buy (Money, market, "put", expirations_mode)
+      check, id=Iq. buy (float(Money), market, "put", int(expirations_mode))
       if check:
-        print("'PUT' Option Placed.") 
-        print("Awaiting 'Put' Option Result...") 
-        await app.send_message(to_chats , "Awaiting 'Call Option Result...")
+        print("IqOption: 'PUT' Option Placed.") 
+        print("IqOption: Awaiting 'Put' Option Result...") 
+        await app.send_message(to_chats , "IqOption: Awaiting 'Call Option Result...")
         #FUNCTION TO GET OPTION RESULT
         if(Iq.check_win_v3(id) !=""):
-          tradeON = False
           trade = ""
-          minutes = ""
+          expirations_mode = ""
           market = ""
-          try:
-            await app.send_message(to_chats , "Win/Loss: " + Iq.check_win_v3(id) + "New Balance: "+str(Iq.get_balance()) )  
-          except Exception as e:
-            print(e)
-          print(Iq.check_win_v3(id))  
+         
+        if(Iq.check_win_v3(id) > 0):
+          print("Win ✅")
+          await app.send_message(to_chats , today+" -IqOption: Win ✅: " + str(float(Iq.check_win_v3(id))) + " IqOption: New Balance: "+str(Iq.get_balance()) )  
+          print(float(Iq.check_win_v3(id))) 
+        elif(Iq.check_win_v3(id) < 0):
+          print("Loss ⛔️")
+          await app.send_message(to_chats , today+" -IqOption: Loss ⛔️: " + str(float(Iq.check_win_v3(id))) + " IqOption: New Balance: "+str(Iq.get_balance()) )  
+          print(float(Iq.check_win_v3(id))) 
       else: 
-        tradeON = False
-        tradeReady == False
-        await app.send_message(to_chats , " 'Put' Option Failed.")
-        print(" 'Put' Option Failed.")
-  
+        await app.send_message(to_chats ,today+" -IqOption: 'Put' Option Failed.")
+        print("IqOption: 'Put' Option Failed.")
 
   #################################
   #print(event.text) #this is the text what is being received and we got is printed!
   #print(event.date) #this is the Date what is being received and we got is printed!
 if __name__ == "__main__":
-	
   print("TGram Bot started!!")
   app.run_until_disconnected()
-  send_msg("TGram Bot started!!")
